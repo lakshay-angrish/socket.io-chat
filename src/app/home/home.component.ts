7c693f17017e11f7768a354682f26c16512d98ec
Lakshay Angrish <angrishlakshay@gmail.com>
2019-07-11 18:42:19 +0530

    click room: go to its chat-room

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import io from 'socket.io-client';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   numberOfRooms = 0;
-  constructor() { }
+  roomName = 'Public';
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
+  createNewRoom() {
+
+  }
 }
